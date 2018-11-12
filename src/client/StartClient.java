5ce12336bae0655039ee1a0dd3f98b7542dc98d5
Anna <arseninaanna@mail.ru>
2018-11-12 04:27:47 +0300

    Added base structure (draft)

---
+++ src/client/StartClient.java
@@ -0,0 +1,39 @@
+package client;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class StartClient {
+
+    static String serverAddr = null;
+    static int serverPort = 8090;
+    static int timeout = 15 * 1000;
+
+    public static void main(String[] args) {
+        try {
+            //Socket soc = new Socket(serverAddr, serverPort);
+            //soc.setSoTimeout(timeout);
+
+            //BufferedInputStream socIn = new BufferedInputStream(soc.getInputStream());
+            //BufferedOutputStream socOut = new BufferedOutputStream(soc.getOutputStream());
+
+            BufferedInputStream sysIn = new BufferedInputStream(System.in);
+            BufferedOutputStream sysOut = new BufferedOutputStream(System.out);
+
+            sysOut.write("asd".getBytes());
+            sysOut.flush();
+        } catch (ConnectException e) {
+            System.out.println("Failed to connect to server. Is it running?");
+        } catch (UnknownHostException e) {
+            System.out.println("Failed to find host. Check ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/client/UserInterface.java
@@ -0,0 +1,19 @@
+package client;
+
+import common.Message;
+
+public class UserInterface {
+
+    public void printSystemMessage(String msg) {
+        // noop
+    }
+
+    public void printMessage(Message msg) {
+        // noop
+    }
+
+    public void onMessageEnter() {
+        // noop
+    }
+
+}
